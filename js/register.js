e67731c7c5154ee81f8d6e788a8100a097672596
李逸飞 <17307130009@fudan.edu.cn>
2020-03-25 17:19:06 +0800

    regisert & login初步实现

--- js/register.js
+++ js/register.js
@@ -9,8 +9,34 @@ var vm = new Vue({
     },
 
     methods: {
-        register : function() {
-            alert(this.id + ' ' + this.pwd + ' ' + this.email)
+        req_register : function() {
+            if(this.id == "" || this.pwd == ""){
+                alert("请输入用户名和密码！")
+            }else{
+                //var json = '{"username" : "' + this.id + '",' + ' "password" : "' + this.pwd + '"}';
+                //alert(json)
+                axios.post('http://127.0.0.1:5000/account/register', data = {
+                    username : this.id,
+                    password : this.pwd
+                }, {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+                .then(response => (this.ack_register(response)))
+                .catch(function(error){
+                    console.log(error);
+                });
+            }
+        },
+
+        ack_register : function(response){
+            var data = response.data;
+            if(data.err_code == 0){
+                alert("恭喜注册成功！请牢记用户名和密码");
+            }else{
+                alert("注册失败\n" + data.message);
+            }
+            location.reload(); 
         }
+
     }
 })
--- js/sign.js
+++ js/sign.js
@@ -8,8 +8,34 @@ var vm = new Vue({
     },
 
     methods: {
-        log_in : function() {
-            alert(this.id + ' ' + this.pwd)
+        req_login : function() {
+            if(this.id == "" || this.pwd == ""){
+                alert("请输入用户名和密码！")
+            }else{
+                //var json = '{"username" : "' + this.id + '",' + ' "password" : "' + this.pwd + '"}';
+                //alert(json)
+                axios.post('http://127.0.0.1:5000/account/login', data = {
+                    username : this.id,
+                    password : this.pwd
+                }, {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+                .then(response => (this.ack_login(response)))
+                .catch(function(error){
+                    console.log(error);
+                });
+            }
+        },
+
+        ack_login : function(response){
+            var data = response.data;
+            if(data.err_code == 0){
+                alert("登录成功！欢迎" + this.id);
+            }else{
+                alert("登录失败\n" + data.message);
+            }
+            location.reload(); 
         }
+
     }
 });
